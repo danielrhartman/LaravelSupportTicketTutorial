d6b446423829cf47b629d1f0873d883d4c8024d3
Daniel R Hartman <danielrhartman@gmail.com>
2018-03-08 16:39:20 +0000

    add ticket comments and admin functions

--- app/Http/Controllers/TicketsController.php
+++ app/Http/Controllers/TicketsController.php
@@ -5,17 +5,29 @@ namespace App\Http\Controllers;
 use App\Category;
 use App\Mailers\AppMailer;
 use App\Ticket;
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class TicketsController extends Controller
 {
 
+    public function __construct() {
+        $this->middleware('auth');
+    }
+
     public function create(){
 
         $categories = Category::all();
 
         return view('tickets.create', compact('categories'));    
+    }
+
+    public function index() {
+        $tickets = Ticket::paginate(10);
+        $categories = Category::all();
+    
+        return view('tickets.index', compact('tickets', 'categories'));
     }
 
     public function store(Request $request, AppMailer $mailer) {
@@ -41,5 +53,35 @@ class TicketsController extends Controller
         $mailer->sendTicketInformation(Auth::user(), $ticket);
 
         return redirect()->back()->with("status", "A ticket with ID: #$ticket->ticket_id has been opened.");
+    }
+
+    public function userTickets() {
+        $tickets = Ticket::where('user_id', Auth::user()->id)->paginate(10);
+        $categories = Category::all();
+
+        return view('tickets.user_tickets', compact('tickets', 'categories'));
+    }
+
+    public function show($ticket_id) {
+        $ticket = Ticket::where('ticket_id', $ticket_id)->firstOrFail();
+
+        $comments = $ticket->comments;
+        $category = $ticket->category;
+
+        return view('tickets.show', compact('ticket', 'category', 'comments'));
+    }
+
+    public function close($ticket_id, AppMailer $mailer) {
+        $ticket = Ticket::where('ticket_id', $ticket_id)->firstOrFail();
+    
+        $ticket->status = 'Closed';
+    
+        $ticket->save();
+    
+        $ticketOwner = $ticket->user;
+    
+        $mailer->sendTicketStatusNotification($ticketOwner, $ticket);
+    
+        return redirect()->back()->with("status", "The ticket has been closed.");
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,13 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('new_ticket', 'TicketsController@create');
+Route::get('my_tickets', 'TicketsController@userTickets');
+Route::get('tickets/{ticket_id}', 'TicketsController@show');
 Route::post('new_ticket', 'TicketsController@store');
+
+Route::post('comment', 'CommentsController@postComment');
+
+Route::group(['prefix' => 'admin', 'middleware' => 'admin'], function() {
+    Route::get('tickets', 'TicketsController@index');
+    Route::post('close_ticket/{ticket_id}', 'TicketsController@close');
+});
